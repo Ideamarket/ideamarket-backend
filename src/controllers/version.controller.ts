ecedfa41cbec1592e10d47420048445d75570e83
Salman Zahid <salmanzahidd@gmail.com>
2022-01-28 01:54:44 +0500

    feat: bump version

--- src/controllers/version.controller.ts
+++ src/controllers/version.controller.ts
@@ -5,7 +5,7 @@ import { handleSuccess, handleError } from '../lib/base'
 export async function fetchVersion(req: Request, res: Response) {
   try {
     // Add Switch API
-    const data = { version: '1.3' }
+    const data = { version: '1.4' }
     return handleSuccess(res, data)
   } catch (error) {
     return handleError(res, error, 'Unable to fetch version')
