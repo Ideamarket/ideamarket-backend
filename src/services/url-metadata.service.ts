9a526cecc14b3a3c3a36f604663b2532ec3c0bd5
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2022-01-30 07:35:42 +0530

    feat: add ogType in url metadata api

--- src/services/url-metadata.service.ts
+++ src/services/url-metadata.service.ts
@@ -1,6 +1,6 @@
 import og from 'ts-open-graph-scraper'
 
-import type { IUrlMetadataModel } from '../models/url-metadata.model'
+import type { IUrlMetadata } from '../models/url-metadata.model'
 import { UrlMetadataModel } from '../models/url-metadata.model'
 
 export function extract(url: string) {
@@ -11,26 +11,27 @@ export async function fetchByUrl(url: string) {
   let data = await UrlMetadataModel.findOne({ url })
 
   if (data && Date.now() < data.expiresAt.getTime()) {
-    return createUrlMetadataResponse(data as IUrlMetadataModel)
+    return createUrlMetadataResponse(data as IUrlMetadata)
   }
 
   const extractRet = await extract(url)
 
-  const { ogTitle, ogDescription, ogImage } = extractRet
+  const { ogTitle, ogDescription, ogImage, ogType } = extractRet
 
   data = await saveUrlMetadata({
     ogImage: ogImage ? ogImage[0].url : undefined,
     ogTitle: ogTitle as string,
     ogDescription: ogDescription as string,
+    ogType: ogType as string,
     favicon: ogImage ? ogImage[0].url : undefined,
     url,
     expiresAt: createExpiryDate(new Date(), 7),
   })
 
-  return createUrlMetadataResponse(data as IUrlMetadataModel)
+  return createUrlMetadataResponse(data as IUrlMetadata)
 }
 
-export function saveUrlMetadata(model: IUrlMetadataModel) {
+export function saveUrlMetadata(model: IUrlMetadata) {
   const query = { url: model.url }
   const options = { upsert: true, new: true, setDefaultsOnInsert: true }
   return UrlMetadataModel.findOneAndUpdate(query, model, options)
@@ -42,10 +43,11 @@ const createExpiryDate = (date: Date, days: number) => {
   return new Date(todayDate.setDate(todayDate.getDate() + days))
 }
 
-const createUrlMetadataResponse = (data: IUrlMetadataModel) => {
+const createUrlMetadataResponse = (data: IUrlMetadata) => {
   return {
     ogTitle: data.ogTitle,
     ogDescription: data.ogDescription,
+    ogType: data.ogType,
     ogImage: data.ogImage,
     favicon: data.favicon,
   }
