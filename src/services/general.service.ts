8c416159abf9a605bb3eb081ddb8c11902c363b8
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2022-02-09 23:27:00 +0530

    fix: add user-agent header while checking valid url

--- src/services/general.service.ts
+++ src/services/general.service.ts
@@ -11,7 +11,12 @@ import cheerio from 'cheerio'
 export async function checkAndReturnValidUrl(url: string) {
   let res: any = null
   try {
-    res = await axios.get(encodeURI(url))
+    res = await axios.get(encodeURI(url), {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36',
+      },
+    })
   } catch (error: any) {
     console.error(
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
