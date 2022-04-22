8e142770807395060f33fce90fe583a030f70d31
Sai Dheeraj <saidheeraj.kumar04@gmail.com>
2022-04-22 22:24:00 +0530

    fix: update ideaTokenVault contract for L1

--- src/web3/contract.ts
+++ src/web3/contract.ts
@@ -7,21 +7,26 @@ import { getDeployedAddresses } from './deployedAddresses'
 
 const NETWORK = config.get<string>('web3.network')
 const L1_NETWORK = getL1Network(NETWORK)
+
 const RPC_URL = config.get<string>(`web3.rpcUrls.${NETWORK}`)
+const L1_RPC_URL = config.get<string>(`web3.rpcUrls.${L1_NETWORK}`)
+
 const PRIVATE_KEY = config.get<string>(`web3.privateKeys.${NETWORK}`)
+const L1_PRIVATE_KEY = config.get<string>(`web3.privateKeys.${L1_NETWORK}`)
 
 const web3 = new Web3(RPC_URL)
+const web3L1 = new Web3(L1_RPC_URL)
 
 /**
  * Returns idea token valut contract for L1 layer
  */
 export function getIdeaTokenVaultContractForL1() {
-  const account = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY)
-  web3.eth.accounts.wallet.add(account)
-  return new web3.eth.Contract(
+  const account = web3L1.eth.accounts.privateKeyToAccount(L1_PRIVATE_KEY)
+  web3L1.eth.accounts.wallet.add(account)
+  return new web3L1.eth.Contract(
     getDeployedABIs(L1_NETWORK).ideaTokenVault as any,
     getDeployedAddresses(L1_NETWORK)?.ideaTokenVault ?? undefined,
-    { from: web3.eth.defaultAccount ?? undefined }
+    { from: web3L1.eth.defaultAccount ?? undefined }
   )
 }
 
